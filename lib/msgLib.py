a05090d135334c3ee1dae015b4c150a6e50b84dc
product1 <manoch@iris.washington.edu>
2018-07-10 07:21:21 -0700

    prerelease updates

--- lib/msgLib.py
+++ lib/msgLib.py
@@ -3,46 +3,41 @@
 #   a collection of functions to print messages
 #
 # HISTORY
+#   2018-07-10 IRIS DMC Product Team (Manoch): prerelease R.2018191
 #   2015-05-19 IRIS DMC Product Team (Manoch): created (R.2015139)
 #
 # NOTES
 # 
 #
 
-#
-# run message
-#
+
 def message(thisMessage):
-   bar = "**********************"
-   prin ("%s %s %s"%(bar,thisMessage,bar))
+    """print a run message"""
+    bar = "*" * 12
+    prin ("%s %s %s"%(bar,thisMessage,bar))
 
-#
-# error
-#
+
 def error(message,code):
-   print("\n[ERROR] %s\n"%(message))
-   return(code)
+    """print an error message"""
+    print("\n[ERROR] %s\n"%(message))
+    return(code)
 
-#
-# warning
-#
+
 def warning(sender,message):
-   print("[WARNING from %s] %s"%(sender,message))
+    """print a warning message"""
+    print("[WARNING from %s] %s"%(sender,message))
 
-#
-# info
-#
+
 def info(message):
-   print("[INFO] %s"%(message))
-   return
+    """print an informative message"""
+    print("[INFO] %s"%(message))
+    return
 
 
-#
-# get a variable from the parameter file by name
-#
 def param(params,var):
-  if var in dir(params):
-    return params
-  code = error("variable '"+var+"' not in the parameter file",code=2)
-  import sys
-  sys.exit(code)
+    """get a variable from the parameter file by name"""
+    if var in dir(params):
+      return params
+    code = error("'".join(["variable ",var," not in the parameter file"]),code=2)
+    import sys
+    sys.exit(code)
