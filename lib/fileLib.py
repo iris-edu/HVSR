14e1268663e4090344e84298b123b5b4b2e3475a
product1 <manoch@iris.washington.edu>
2018-07-10 07:21:14 -0700

    prerelease updates

--- lib/fileLib.py
+++ lib/fileLib.py
@@ -3,6 +3,7 @@
 #   a collection of functions to work with files and directories
 #
 # HISTORY
+#   2018-07-10 IRIS DMC Product Team (Manoch): prerelease R.2018191
 #   2015-05-19 IRIS DMC Product Team (Manoch): created (R.2015139)
 #
 # NOTES
@@ -16,10 +17,9 @@ sys.path.append(libraryPath)
 
 import msgLib 
 
-#
-# mkdir
-#
+
 def mkdir(baseDir,dir):
+   """ make a directory"""
    try:
       thisDir = os.path.join(baseDir,dir)
       if not os.path.exists(thisDir):
@@ -28,16 +28,14 @@ def mkdir(baseDir,dir):
    except:
       return msgLib.error("failed to create directory "+thisDir,None)
 
-#
-# Baseline file name
-#
+
 def baselineFileName(network,station,location,channel):
+   """construct the baseline file name"""
    label   = '.'.join([network,station,location,channel,'txt'])
    return label
 
-#
-# HVSR file name
-#
+
 def hvsrFileName(network,station,location,start,end):
+   """construct the HVSR file name"""
    label   = '.'.join([network,station,location,start,end,'HVSR','txt'])
    return label
