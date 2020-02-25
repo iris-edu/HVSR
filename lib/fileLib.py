94b714825a74057ddc89761be2ba3737ef5b528c
manoch <manoch@iris.washington.edu>
2020-02-25 08:10:44 -0800

    V.2020.055, style update

--- lib/fileLib.py
+++ lib/fileLib.py
@@ -3,6 +3,7 @@
     a collection of functions to work with files and directories
 
   HISTORY
+    2020-02-24 IRIS DMC Product Team (Manoch): style update V.2020.055
     2019-06-03 IRIS DMC Product Team (Manoch): release V.2019.154
     2018-07-10 IRIS DMC Product Team (Manoch): pre-release V.2018.191
     2015-05-19 IRIS DMC Product Team (Manoch): created (V.2015.139)
@@ -11,31 +12,31 @@
 import os
 import sys
 
-hvsrDirectory   = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-libraryPath     = os.path.join(hvsrDirectory, 'lib')
-sys.path.append(libraryPath)
+hvsr_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+library_path = os.path.join(hvsr_directory, 'lib')
+sys.path.append(library_path)
 
-import msgLib 
+import msgLib
 
 
-def mkdir(baseDir, dir):
-   """ make a directory"""
-   try:
-      thisDir = os.path.join(baseDir, dir)
-      if not os.path.exists(thisDir):
-         os.makedirs(thisDir)
-      return thisDir
-   except:
-      return msgLib.error('failed to create directory {}'.format(thisDir), None)
+def mkdir(base_directory, target_directory):
+    """ Make a directory if it does not exist."""
+    try:
+        _directory = os.path.join(base_directory, target_directory)
+        if not os.path.exists(_directory):
+            os.makedirs(_directory)
+        return _directory
+    except Exception as _er:
+        return msgLib.error(f'failed to create directory {_directory}\n{_er}', None)
 
 
 def baselineFileName(network, station, location, channel):
-   """construct the baseline file name"""
-   label   = '.'.join([network, station, location, channel, 'txt'])
+   """Construct the baseline file name."""
+   label = '.'.join([network, station, location, channel, 'txt'])
    return label
 
 
-def hvsrFileName(network,station,location,start,end):
-   """construct the HVSR file name"""
-   label   = '.'.join([network, station, location, start, end, 'HVSR','txt'])
+def hvsrFileName(network, station, location, start, end):
+   """Construct the HVSR file name."""
+   label = '.'.join([network, station, location, start, end, 'HVSR', 'txt'])
    return label
